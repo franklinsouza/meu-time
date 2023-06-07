2cb227d5a878b95c915501bd619dc98dd8c62f75
Franklin Souza <frankrsouza@gmail.com>
2023-06-07 14:38:34 -0300

    feat: configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      primary: {
-        '01': '#D41640',
-      },
-    },
     fontFamily: {
       sans: ['Josefin Sans', 'sans-serif'],
     },
     extend: {
       backgroundImage: {
         body: "url('assets/bg.png');",
+        dashboard: "url('assets/bg-dashboard.png');",
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: {
+          '01': '#D41640',
+        },
+        secondary: {
+          '01': '#1D1B24',
+        },
       },
     },
   },
